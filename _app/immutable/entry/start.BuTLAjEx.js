cf96dd30fd939e52e7750f2f469ab251c3a0746f
John Jacques <eagleevil@gmail.com>
2024-03-04 00:35:53 -0600

    testing sveltekit static on github pages

---
+++ _app/immutable/entry/start.BuTLAjEx.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CqBvB1bx.js";export{t as start};
---
+++ _app/immutable/nodes/2.CJJCj5oJ.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.DmhBjxsz.js";import{S as t,i as e}from"../chunks/index.CiwmKfKN.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
